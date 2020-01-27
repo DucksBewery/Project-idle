1e2cc4be85b739083aeaeaec7e27066a15a3080b
BOUCHON <valcrowdu81@hotmail.fr>
2020-01-27 10:43:33 +0100

    Assign Duck working with slots

---
+++ Assets/Scripts/AssignedCard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AssignedCard : MonoBehaviour
+{
+    public Duck duck;
+    private Town town;
+    private Menu_Manager menu;
+
+    public Text UILevel;
+    public Text UILife;
+    public Text UIStrength;
+    public Text UIEndurance;
+    public Text UIAgility;
+    public Text UIInteligence;
+    //public Text UIActualJob;
+
+    private void Start()
+    {
+        if (duck != null)
+        {
+            UILevel.text = duck.dLevel.ToString();
+            UILife.text = duck.life.ToString(); ;
+            UIStrength.text = duck.strength.ToString(); ;
+            UIEndurance.text = duck.endurance.ToString(); ;
+            UIAgility.text = duck.agility.ToString(); ;
+            UIInteligence.text = duck.inteligence.ToString(); ;
+            //UITargetJob.text = duck.ToString(); ;
+            //UIActualJob.text = duck.dLevel.ToString();
+        }
+        town = FindObjectOfType<Town>();
+        menu = FindObjectOfType<Menu_Manager>();
+    }
+
+    public void TargetSlot()
+    {
+        town.TargetSlot(Int32.Parse(gameObject.name));
+        menu.GoToResourceBuildingsDuckSelectionView();
+        town.DisplayCards();
+    }
+}
--- Assets/Scripts/Town.cs
+++ Assets/Scripts/Town.cs
@@ -127,5 +127,157 @@ public class Town : MonoBehaviour
 
         ui.RefreshResources();
 
+<<<<<<< Updated upstream
+=======
+    public void UpgradeBuilding(int buildingId)
+    {
+        if (golds.Amount >= buildings[buildingId].bPrice) buildings[buildingId].Upgrade();
+
+        //Saving changes
+        loadedSave.SaveTown();
+    }
+
+    public void UpgradeResource(Resource resource)
+    {
+        if (golds.Amount >= resource.rPrice) resource.Upgrade();
+
+        //Saving changes
+        loadedSave.SaveTown();
+    }
+
+    //TO DO : Recuperer infos de quelle case/quel building au toucher
+    public void TargetBuildingAssignView(int targetBuildingToAssignTemp)
+    {
+        targetBuilding = buildings[targetBuildingToAssignTemp];
+        DisplayAssignedCards();
+    }
+
+    public void TargetSlot(int targetWorkerSlotToAssignTemp)
+    {
+        print(targetWorkerSlotToAssignTemp);
+        targetWorkerSlot = targetWorkerSlotToAssignTemp;
+    }
+
+    public void AssignWorker(Duck targetWorker)
+    {
+        targetBuilding.AssignWorker(targetWorkerSlot, targetWorker);
+    }
+
+    public void UnassignWorker(Duck targetWorker)
+    {
+        targetBuilding.AssignWorker(targetWorkerSlot, targetWorker);
+    }
+
+    public void CreateDuck()
+    {
+        //Duck newDuck = new Duck(ducks.Length);
+        /*
+        GameObject test = new GameObject();
+        test.name = "Duck_n#" + ducks.Count;
+        Duck newDuck = test.AddComponent<Duck>();
+        newDuck.dId = ducks.Count;
+        */
+        Duck newDuck = new Duck(ducks.Count);
+
+        print(newDuck.life);
+        print(newDuck.endurance);
+        print(newDuck.strength);
+        print(newDuck.agility);
+
+        ducks.Add(newDuck);
+        loadedSave.SaveTown();
+    }
+
+    IEnumerator Idle()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            if (bank.workers.Length > 0)            golds.Increment((bank.workersCoefficient));
+            if (well.workers.Length > 0)            water.Increment((well.workersCoefficient));
+            if (croutonFields.workers.Length > 0)   food.Increment((croutonFields.workersCoefficient));
+            if (lumbermill.workers.Length > 0)      wood.Increment((lumbermill.workersCoefficient));
+            if (manaWell.workers.Length > 0)        mana.Increment((manaWell.workersCoefficient));
+            ui.RefreshResources();
+
+            //Saving changes
+            loadedSave.SaveTown();
+        }
+    }
+
+    public void DisplayCards()
+    {
+        print("test");
+        print(ducks.Count);
+        foreach (Duck duck in ducks)
+        {
+            //Setting Card on scene
+            GameObject newCard = Instantiate(cardComponent, cardGrid.transform);
+            newCard.name = duck.dId.ToString();
+
+            //Adding Card script and selected Duck
+            newCard.GetComponent<Card>().duck = duck;
+            displayedDuckCards.Add(newCard.GetComponent<Card>());
+        }
+    }
+
+    public void DisplayAssignedCards()
+    {
+        //TO DO : A modif pour afficher un les cards en fonction de leur position
+        /*
+        int i = 0;
+        foreach (Duck duck in targetBuilding.workers.Where(itm => itm != null))
+        {
+            //Setting Card on scene
+            GameObject newAssignedCard = Instantiate(assignedCardComponent, assignedCardGrid.transform);
+            newAssignedCard.name = duck.dId.ToString();
+
+            //Adding Card script and selected Duck
+            newAssignedCard.GetComponent<AssignedCard>().duck = duck;
+            newAssignedCard.GetComponent<AssignedCard>().targetSlot = i;
+            displayedAssignedDuckCards.Add(newAssignedCard.GetComponent<AssignedCard>());
+
+            i++;
+        }*/
+
+        
+        for (int i=0; i<targetBuilding.workers.Length; i++)
+        {
+            if (targetBuilding.workers[i] != null)
+            {
+                //Setting Card on scene
+                GameObject newAssignedCard = Instantiate(assignedCardComponent, assignedCardGrid.transform);
+                newAssignedCard.name = i.ToString();
+
+                //Adding Card script and selected Duck
+                newAssignedCard.GetComponent<AssignedCard>().duck = targetBuilding.workers[i];
+                displayedAssignedDuckCards.Add(newAssignedCard.GetComponent<AssignedCard>());
+            }
+            else
+            {
+                //Setting Card on scene
+                GameObject newAssignedCard = Instantiate(assignedCardComponent, assignedCardGrid.transform);
+                newAssignedCard.name = i.ToString();
+                //Adding Card script and selected Duck
+                displayedAssignedDuckCards.Add(newAssignedCard.GetComponent<AssignedCard>());
+            }
+        }
+    }
+
+    public void DestroyDisplayedCards()
+    {
+        foreach (Card child in cardGrid.GetComponentsInChildren<Card>())
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    public void DestroyDisplayedAssignedCards()
+    {
+        foreach (AssignedCard child in assignedCardGrid.GetComponentsInChildren<AssignedCard>())
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+>>>>>>> Stashed changes
     }
 }
